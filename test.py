d690267b91413dccc984552828bc4d2fb7a24408
John Wu <johnwu3412@gmail.com>
2023-01-01 23:39:30 -0500

    fixing up test.py, get mastery and stuff

--- test.py
+++ test.py
@@ -1,12 +1,25 @@
 import requests
 import json
 
+# variables
 base_url = 'https://na1.api.riotgames.com/'
-api_endpoint = 'lol/summoner/v4/summoners/by-name/'
+get_summoner_api_endpoint = 'lol/summoner/v4/summoners/by-name/'
+get_champion_mastery_endpoint = 'lol/champion-mastery/v4/champion-masteries/by-summoner/'
 key = 'RGAPI-16399abc-5203-46b0-a907-ec20409bba6b'
 
+#input for summoner name
 summoner_name =  input("Enter your summoner name: ")
 
-get_summoner = base_url + api_endpoint + summoner_name + '?api_key=' + key
-print(requests.get(get_summoner).text)
- 
+#constructing API url to get account id of a summoner
+get_summoner = base_url + get_summoner_api_endpoint + summoner_name + '?api_key=' + key
+
+#sending request and reformatting request -> todo: need to add error cases
+summoner = json.loads(requests.get(get_summoner).text)
+
+#constructing API url to get champion mastery of a summoner
+get_mastery = base_url + get_champion_mastery_endpoint + summoner["id"] + '?api_key=' + key
+
+#sending request and reformatting request -> todo: need to add error cases
+mastery = json.loads(requests.get(get_mastery).text)
+
+print(mastery)
