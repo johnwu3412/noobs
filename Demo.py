c99aa47d2721695527b491f6d6c416fe10269717
Charlie Zhang <czhang10@umd.edu>
2023-01-11 20:11:56 -0500

    Flask Folder

--- Demo.py
+++
@@ -1,5 +0,0 @@
-import requests
-import json
-#url = "https://www.albion-online-data.com/"
-
-print(json.loads(requests.get("https://www.albion-online-data.com/api/v2/stats/prices/T4_BAG,T5_BAG.json?locations=Caerleon,Bridgewatch&qualities=2%22").text))
