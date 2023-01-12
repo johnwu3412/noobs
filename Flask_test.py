0726a669e684bab675e759766164e9656a89262c
Charlie Zhang <czhang10@umd.edu>
2023-01-11 20:07:01 -0500

    Flask testing

---
+++ Flask_test.py
@@ -0,0 +1,18 @@
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+    
+@app.route("/")
+def home():
+    return "Hello"
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"))
+
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
+
+if __name__ == "__main__":
+    app.run()
