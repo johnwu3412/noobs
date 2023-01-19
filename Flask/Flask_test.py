5f71e66b4590d578789c97690f1655768c3c204a
Charlie Zhang <czhang10@umd.edu>
2023-01-18 21:45:20 -0500

    Riot Client and Flask link established

--- Flask/Flask_test.py
+++ Flask/Flask_test.py
@@ -1,26 +1,17 @@
 #pip install flask
-
 from flask import Flask, redirect, url_for
-
 import sys
-
-sys.path.insert(0, r'C:\Users\Kenny\Documents\Project 2022\noobs')
-
-#from RiotClient import get_masteries_by_summoner, get_summoner_by_name
-
-from modules.RiotClient import RiotClient
-
 import os
+sys.path.insert(0, r'C:\Users\Charlie\Desktop\Coding Stuff\noobs')
+from modules.RiotClient import RiotClient
 
 app = Flask(__name__)
 
-
-
 rc = RiotClient("https://na1.api.riotgames.com/",os.getenv("RiotGamesKey"))
-
+summonerID = rc.get_summoner_by_name("ShabobNiqqua")
 @app.route("/")
 def home():
-    return test()
+    return rc.get_masteries_by_summoner(summonerID)
 
 @app.route("/admin")
 def admin():
