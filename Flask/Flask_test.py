5192e2075624c92c768258d4d9866a8046c95781
John Wu <johnwu3412@gmail.com>
2023-02-08 20:24:16 -0500

    /insultme enpoint -- insults list api added to backend

--- Flask/Flask_test.py
+++ Flask/Flask_test.py
@@ -1,7 +1,11 @@
 #pip install flask
 
 #           ***Imports***
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, request
+from flask_restful import Api, Resource, reqparse
+import json
+# Api Class-> makes the flask app a Restful API
+
 import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(sys.path[0]), 'modules'))
@@ -9,11 +13,22 @@ from RiotClient import RiotClient
 
 #           ***Initializing***
 app = Flask(__name__)
+api = Api(app)
 
 rc = RiotClient("https://na1.api.riotgames.com/",os.getenv("RiotGamesKey"))
-summonerID = rc.get_summoner_by_name("ShabobNiqqua")
+#summonerID = rc.get_summoner_by_name("ShabobNiqqua")
 
 requests = [""]
+insults = {}
+
+class InsultsList(Resource):
+    def get(self):
+        return json.dumps(insults), 200
+    def post(self):
+        data = request.get_json()
+        insults[data["name"]]= data["value"]
+        return 200
+
 
 #           ***Processing***
 @app.route("/")
@@ -28,6 +43,9 @@ def admin():
 def user(name):
     return f"Hello {name}!"
 
+api.add_resource(InsultsList, '/insultme')
+
+#
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
